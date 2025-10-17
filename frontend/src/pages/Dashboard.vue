04c4cf662091d18a402a8ea6d8cc37a2590e70f7
RENPWN <46776762+hardknockdays@users.noreply.github.com>
2025-10-17 16:09:57 +0700

    up

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-6 max-w-6xl mx-auto">
-    <h1 class="text-3xl font-bold mb-6 text-center">Job Order List</h1>
+  <div class="p-6 max-w-6xl mx-auto bg-black min-h-screen text-white">
+    <h1 class="text-3xl font-bold mb-6 text-center text-white">Job Order List</h1>
 
     <!-- Search -->
     <div class="mb-4 flex justify-end">
@@ -8,56 +8,56 @@
         v-model="search"
         type="text"
         placeholder="Search Customer..."
-        class="border border-gray-300 rounded px-3 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        class="border border-gray-700 rounded px-3 py-2 w-64 bg-gray-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
     </div>
 
     <!-- Table -->
     <div class="overflow-x-auto">
-      <table class="min-w-full border-collapse border border-gray-300">
-        <thead class="bg-gray-100">
+      <table class="min-w-full border-collapse border border-gray-700">
+        <thead class="bg-gray-800">
           <tr>
             <th
-              class="border border-gray-300 p-2 text-left cursor-pointer"
+              class="border border-gray-700 p-2 text-left cursor-pointer hover:bg-gray-700"
               @click="sortBy('customer_name')"
             >
               Customer
             </th>
             <th
-              class="border border-gray-300 p-2 text-left cursor-pointer"
+              class="border border-gray-700 p-2 text-left cursor-pointer hover:bg-gray-700"
               @click="sortBy('origin')"
             >
               Origin
             </th>
             <th
-              class="border border-gray-300 p-2 text-left cursor-pointer"
+              class="border border-gray-700 p-2 text-left cursor-pointer hover:bg-gray-700"
               @click="sortBy('destination')"
             >
               Destination
             </th>
             <th
-              class="border border-gray-300 p-2 text-left cursor-pointer"
+              class="border border-gray-700 p-2 text-left cursor-pointer hover:bg-gray-700"
               @click="sortBy('status')"
             >
               Status
             </th>
-            <th class="border border-gray-300 p-2 text-left">Action</th>
+            <th class="border border-gray-700 p-2 text-left">Action</th>
           </tr>
         </thead>
         <tbody>
           <tr
             v-for="jo in filteredJobOrders"
             :key="jo.id"
-            class="hover:bg-gray-50 even:bg-gray-50"
+            class="hover:bg-gray-900 even:bg-gray-900"
           >
-            <td class="border border-gray-300 p-2">{{ jo.customer_name }}</td>
-            <td class="border border-gray-300 p-2">{{ jo.origin }}</td>
-            <td class="border border-gray-300 p-2">{{ jo.destination }}</td>
-            <td class="border border-gray-300 p-2">{{ jo.status }}</td>
-            <td class="border border-gray-300 p-2">
+            <td class="border border-gray-700 p-2">{{ jo.customer_name }}</td>
+            <td class="border border-gray-700 p-2">{{ jo.origin }}</td>
+            <td class="border border-gray-700 p-2">{{ jo.destination }}</td>
+            <td class="border border-gray-700 p-2">{{ jo.status }}</td>
+            <td class="border border-gray-700 p-2">
               <router-link
                 :to="`/job/${jo.id}`"
-                class="text-blue-600 hover:underline"
+                class="text-blue-400 hover:underline"
                 >Detail</router-link
               >
             </td>
